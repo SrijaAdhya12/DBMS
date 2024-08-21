5ed1e611bfdd2fde7637475fb63667cbfdd266ab
Srija Adhya <srijaadhya.11sc.2020@gmail.com>
2024-08-22 02:39:22 +0530

    feat: added assignment 1

---
+++ assignments/Assignment-1/Select2Col.sql
@@ -0,0 +1,3 @@
+-- Q3 Select 2 cols from the table DEPTCST:
+
+SELECT SNO, NAME FROM DEPTCST;
---
+++ assignments/Assignment-1/SelectMarks.sql
@@ -0,0 +1,3 @@
+-- Q4 Select Student Marks greater than 80
+
+SELECT NAME, MARKS FROM DEPTCST WHERE MARKS > 80;
