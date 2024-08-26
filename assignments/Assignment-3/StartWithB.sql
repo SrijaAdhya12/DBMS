7ebeb38ca13bb37fea93d94713b990b108ff07e1
Srija Adhya <srijaadhya.11sc.2020@gmail.com>
2024-08-26 23:39:38 +0530

    Update StartWithB.sql

--- assignments/Assignment-3/StartWithB.sql
+++ assignments/Assignment-3/StartWithB.sql
@@ -1,3 +1,3 @@
 -- Q8 List all the employees those are starting with B
 
-ELECT SUBSTR(E_name) AS ShortName FROM EMPLOYEE WHERE E_name LIKE 'B%'
+SELECT SUBSTR(E_name) AS ShortName FROM EMPLOYEE WHERE E_name LIKE 'B%'
